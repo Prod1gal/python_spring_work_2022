6c6fa3ace32aa27c8e452039c1fc5960ab65ce0f
Juliana <86745990+Prod1gal@users.noreply.github.com>
2022-04-11 13:45:33 +0300

    Загружены таски с домашней работой

---
+++ unit_one/lesson__2__08.04.2022/task/task3.py
@@ -0,0 +1,18 @@
+##todo: Самостоятельная работа
+
+# Написать программу, которая считывает два числа и выводит их сумму, разность, частное, произведение,
+# результат целочисленного деления, результат деления с остатком, результат возведения с остатком.
+
+# x = 10
+# print(x)
+
+number_one = input()
+number_two = input()
+
+print(int(number_one) + int(number_two))
+print(int(number_one) - int(number_two))
+print(int(number_one) / int(number_two))
+print(int(number_one) * int(number_two))
+print(float(number_one) // float(number_two))
+print(int(number_one) % int(number_two))
+print(float(number_one) ** float(number_two))
